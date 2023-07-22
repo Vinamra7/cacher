647de7f3c767e424836c3127f4278733507ddcff
Vinamra Mishra <vinamramishra2011@gmail.com>
2023-07-22 19:37:21 +0530

    API for automated VM deploment

--- controller/azure_controller.js
+++ controller/azure_controller.js
@@ -1,85 +1,31 @@
-const msRestNodeAuth = require("@azure/ms-rest-nodeauth");
-const { ComputeManagementClient } = require("@azure/arm-compute");
+const { spawn } = require('child_process');
 
-// const clientId = "eacfad3e-a728-4059-aa2d-2b4f7319776e";
-// const tenantId = "12b4fbf9-dea8-4490-bede-9cc40309ad61";
-// const secret = "2Rh8Q~f57ujrRdNsLuDBdkn~zguPQHFEsvNPXc26";
-// const subscriptionId = "9ec5dfe7-ab9e-4f9b-ade0-ffb59f14777a";
-// const resourceGroupName = "test-project_group";
-// const adminUsername = "test-project";
-// const adminPassword = "aA1123456789";
-// const vmName = "test-project2";
-// const vmSize = "Standard_B1s";
+
 const increaseCachingCapacityController = async (req, res) => {
-  const subscriptionId = "9ec5dfe7-ab9e-4f9b-ade0-ffb59f14777a";
+  const scriptPath = `scripts\\vm_deploy.ps1`;
 
-  const resourceGroupName = "test-project_group";
+  // Run PowerShell with the script as an argument
+  const child = spawn('powershell.exe', [scriptPath], {
+    stdio: 'pipe' // Set 'pipe' for stdout to capture the output
+  });
 
-  const vmName = "test-project2";
+  // Capture and display the output of the script
+  let result = '', error = '';
+  child.stdout.on('data', (data) => {
+    console.log(`PowerShell Output:\n${data}`);
+    result = data;
+  });
 
-  const clientId = "eacfad3e-a728-4059-aa2d-2b4f7319776e";
+  // Handle any errors that occur during script execution
+  child.on('error', (err) => {
+    console.error(`Error executing PowerShell script: ${err}`);
+    error = err;
+  });
 
-  const tenantId = "12b4fbf9-dea8-4490-bede-9cc40309ad61";
-
-  const secret = "QiU8Q~rytIqSN_ZSN-43sztOf6LMB5QACnQ9Db_t";
-
-  const credential = await msRestNodeAuth.loginWithServicePrincipalSecret(
-    clientId,
-
-    secret,
-
-    tenantId
-  );
-
-  const computeClient = new ComputeManagementClient(credential, subscriptionId);
-
-  const vmParameters = {
-    location: "centralindia",
-
-    hardwareProfile: {
-      vmSize: "Standard_B1s",
-    },
-
-    storageProfile: {
-      imageReference: {
-        sku: "20_04-lts-gen2",
-
-        publisher: "canonical",
-
-        version: "latest",
-
-        offer: "0001-com-ubuntu-server-focal",
-      },
-
-      osDisk: {
-        caching: "ReadWrite",
-
-        managedDisk: {
-          storageAccountType: "Standard_LRS",
-        },
-      },
-    },
-
-    osProfile: {
-      adminUsername: "test-project",
-      computerName: vmName,
-      adminPassword: "aA1123456789",
-    },
-
-  };
-
-  //
-
-  console.log(`Creating virtual machine "${vmName}"...`);
-
-  await computeClient.virtualMachines.beginCreateOrUpdate(
-    resourceGroupName,
-
-    vmName,
-
-    vmParameters
-  );
-
-  console.log(`Virtual machine "${vmName}" created successfully.`);
+  // Handle the script's exit event
+  child.on('exit', (code) => {
+    console.log(`PowerShell script exited with code ${code}`);
+    res.send({ result: result, error: error });
+  });
 };
 module.exports = { increaseCachingCapacityController };
---
+++ scripts/vm_deploy.ps1
@@ -0,0 +1,9 @@
+az login --service-principal --username "eacfad3e-a728-4059-aa2d-2b4f7319776e" --password "2Rh8Q~f57ujrRdNsLuDBdkn~zguPQHFEsvNPXc26" --tenant "12b4fbf9-dea8-4490-bede-9cc40309ad61"
+az vm create --resource-group "test-project_group" `
+  --name "test-project2" `
+  --image "UbuntuLTS" `
+  --admin-username "test-project" `
+  --admin-password "aA1123456789" `
+  --size "Standard_B1s" `
+  --location "centralindia"
+# write-host "Creating resource group: test-project_group"
