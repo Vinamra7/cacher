04d355e612350fe19d050a7e78350239c2e6eb75
Ratul Dawar <ratuldawar11@gmail.com>
2023-07-25 17:29:38 +0530

    Added vm creation through image

--- scripts/vm_deploy.ps1
+++ scripts/vm_deploy.ps1
@@ -4,11 +4,10 @@ param(
 az login --service-principal --username "eacfad3e-a728-4059-aa2d-2b4f7319776e" --password "2Rh8Q~f57ujrRdNsLuDBdkn~zguPQHFEsvNPXc26" --tenant "12b4fbf9-dea8-4490-bede-9cc40309ad61"
 
 az vm create --resource-group "test-project_group" `
-  --name "test-project$countOfServers" `
-  --image "UbuntuLTS" `
+  --name "test-project2" `
+  --image "/subscriptions/9ec5dfe7-ab9e-4f9b-ade0-ffb59f14777a/resourceGroups/test-project_group/providers/Microsoft.Compute/galleries/CacherNew/images/CacherImageDefination/versions/1.0.0" `
   --admin-username "test-project" `
   --admin-password "aA1123456789" `
-  --size "Standard_B1s" `
-  --location "centralindia"
+  --size "Standard_B1s"
 # Replace 'test-project0NSG' with the name of your NSG
 az network nsg rule create --resource-group "test-project_group" --nsg-name "test-project0NSG" --name "AllowAnyCustom8000Inbound" --protocol Tcp --direction Inbound --priority 100 --destination-port-ranges 8000 --access Allow --source-address-prefix "*" --destination-address-prefix "*"
