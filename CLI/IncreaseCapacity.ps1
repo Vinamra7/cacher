93ec4117e33282eba70019d73a35d480a90ae3c9
Vinamra Mishra <vinamramishra2011@gmail.com>
2023-11-27 12:45:56 +0530

    increase capacity CLI interface

---
+++ CLI/IncreaseCapacity.ps1
@@ -0,0 +1,34 @@
+# Function to increase user capacity
+function Set-Capacity {
+   param(
+      [Parameter(Mandatory = $true)]
+      [int]$capacity
+   )
+
+   # Retrieve email from local storage (replace with your actual code)
+   $email = Get-Item -Path HKCU:\Software\Cacher -Name Email
+
+   # Check if email is present in local storage
+   if ($email) {
+      # API endpoint
+      $apiUrl = "http://localhost:3000/user/increase-allocation"
+
+      # Prepare data for API request
+      $requestData = @{
+         email    = $email
+         capacity = $capacity
+      }
+
+      # Convert data to JSON
+      $jsonRequestData = $requestData | ConvertTo-Json
+
+      # Send request to API
+      $response = Invoke-RestMethod -Uri $apiUrl -Method Post -Body $jsonRequestData -ContentType 'application/json'
+
+      # Process API response as needed
+      Write-Host "API Response: $($response | ConvertTo-Json -Depth 5)"
+   }
+   else {
+      Write-Host "Email not found in local storage."
+   }
+}
